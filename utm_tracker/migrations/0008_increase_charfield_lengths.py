68b3794ac24ff2e7b1b25d9ecdbcfdc39e95eacd
Hugo Rodger-Brown <hugo@yunojuno.com>
2022-03-02 19:04:40 +0000

    Fix formatting / linting issues

--- utm_tracker/migrations/0008_increase_charfield_lengths.py
+++ utm_tracker/migrations/0008_increase_charfield_lengths.py
@@ -6,28 +6,40 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('utm_tracker', '0007_alter_leadsource_content'),
+        ("utm_tracker", "0007_alter_leadsource_content"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='leadsource',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="leadsource",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
         migrations.AlterField(
-            model_name='leadsource',
-            name='medium',
-            field=models.CharField(help_text='utm_medium: Identifies what type of link was used, such as cost per click or email.', max_length=100),
+            model_name="leadsource",
+            name="medium",
+            field=models.CharField(
+                help_text="utm_medium: Identifies what type of link was used, such as cost per click or email.",
+                max_length=100,
+            ),
         ),
         migrations.AlterField(
-            model_name='leadsource',
-            name='source',
-            field=models.CharField(help_text='utm_source: Identifies which site sent the traffic, and is a required parameter.', max_length=100),
+            model_name="leadsource",
+            name="source",
+            field=models.CharField(
+                help_text="utm_source: Identifies which site sent the traffic, and is a required parameter.",
+                max_length=100,
+            ),
         ),
         migrations.AlterField(
-            model_name='leadsource',
-            name='term',
-            field=models.CharField(blank=True, help_text='utm_term: Identifies search terms.', max_length=100),
+            model_name="leadsource",
+            name="term",
+            field=models.CharField(
+                blank=True,
+                help_text="utm_term: Identifies search terms.",
+                max_length=100,
+            ),
         ),
     ]
